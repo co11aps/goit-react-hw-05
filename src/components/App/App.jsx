a59788f0a808a3d8c6d1cc5b81b5a744303e8d1b
co11aps <143988142+co11aps@users.noreply.github.com>
2024-04-13 17:21:59 +0800

    11

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,18 @@
-import "./App.css";
+import { fetchMovies } from "../API/API";
+// import css from "./App.module.css";
 
-const App = () => {};
+const App = () => {
+  const handleClick = async () => {
+    await fetchMovies();
+    console.log("hello");
+  };
+
+  return (
+    <>
+      <h1>Main page</h1>
+      <button onClick={handleClick}>Get</button>
+    </>
+  );
+};
 
 export default App;
